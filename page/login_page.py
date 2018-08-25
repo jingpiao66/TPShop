7467b4e8d52f7f10fc7783c5c9c8466ffadd9753
jingpiao66 <paipai66@163.com>
2018-08-25 16:06:36 +0800

    TPshop

--- page/login_page.py
+++ page/login_page.py
@@ -1,3 +1,4 @@
+import allure
 from selenium.webdriver.common.by import By
 from base.base_action import BaseAction
 
@@ -8,14 +9,18 @@ class LoginPage(BaseAction):
     pwd = By.ID, "com.tpshop.malls:id/edit_password"
     login_btn = By.ID, "com.tpshop.malls:id/btn_login"
 
+    @allure.step(title="输入用户名")
     def input_username(self, text):
         self.input(self.username, text)
 
+    @allure.step(title="输入密码")
     def input_pwd(self, password):
         self.input(self.pwd, password)
 
+    @allure.step(title="点击登录按钮")
     def click_login_btn(self):
         self.click(self.login_btn)
 
+    @allure.step(title="判断登录按钮是否可用")
     def is_login_btn_enabled(self):
         self.is_location_enabled(self.login_btn)
--- page/mine_page.py
+++ page/mine_page.py
@@ -1,3 +1,4 @@
+import allure
 from selenium.webdriver.common.by import By
 from base.base_action import BaseAction
 
@@ -5,5 +6,6 @@ from base.base_action import BaseAction
 class MinePage(BaseAction):
     login_sign_up_btn = By.ID, "com.tpshop.malls:id/nickname_txtv"
 
+    @allure.step(title="点击登录/注册")
     def click_login_sign_up_btn(self):
         self.click(self.login_sign_up_btn)
